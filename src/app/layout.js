93353bc2a917c77db3889f9d4eaeaf8a61567ab8
Gustavo Henz <gstavohnz@gmail.com>
2024-01-13 21:31:48 -0300

    incluindo imagens, fonts e cores

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,29 @@
-import { Inter } from 'next/font/google'
+import { Inter, Poppins } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+  variable: '--font-inter'
+})
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['600'],
+  display: 'swap',
+  variable: '--font-poppins'
+})
 
 export const metadata = {
-  title: 'NFTBoost',
+  title: 'NFTBoost | Explore um Universo Digital de Colecionáveis Únicos',
   description: 'Projeto de estudo curso CodeBoost',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${poppins.variable}`}>{children}</body>
     </html>
   )
 }
